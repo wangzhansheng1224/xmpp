ad6df8ba7201a27231d840afea8f44aaff81a61a
wangzs <wangzs@smartdot.com>
2016-12-29 13:30:31 +0800

    初版

---
+++ xmpp/xmpp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmpp
+//
+//  Created by 王战胜 on 2016/12/29.
+//  Copyright © 2016年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmpp/xmpp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xmpp
+//
+//  Created by 王战胜 on 2016/12/29.
+//  Copyright © 2016年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
